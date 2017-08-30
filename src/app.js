57d4ffb27ab9dcbede837d7279eaacf59c7bfca9
John Doe <johndoe@example.com>
2017-08-30 10:12:34 +0700

    Signed-off-by: John Doe <johndoe@example.com>

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const openStream = require('./openStream');
+const playVideo = require('./playVideo');
+const Peer = require('peerjs');
+const uid = require('uid');
+const $ = require('jquery');
+const config = {host: 'streamlinhrtc.herokuapp.com',port: 443,secure:true,key:'peerjs'};
+function getPeer() {
+	const id = uid(10);
+	$('#peer-id').append(id);
+	return id;
+}
+const peer = Peer(getPeer(),config);
+console.log(peer);
+$('#btnCall').click( () => {
+	const friendId = $('#txtFriendId').val();
+	openStream(stream => {
+		playVideo(stream,'localStream');
+		const call = peer.call(friendId,stream);
+		call.on('stream',remoteStream => playVideo(remoteStream, 'friendStream'));
+	});
+});
+peer.on('call', (call) => {
+ openStream(stream => {
+		playVideo(stream,'localStream');
+		call.answer(stream);
+		call.on('stream',remoteStream => playVideo(remoteStream, 'friendStream'));
+	});
+});
